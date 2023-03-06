7f0d2a461c76beba24ee634040d38313701f3741
sohail <sohailk1999@gmail.com>
2023-03-07 00:59:06 +0530

    ok

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -36,7 +36,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         create_user(**payload)
-        res = self.client.get(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
     
     def test_password_too_short(self):
