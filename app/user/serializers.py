14e6fea9551e4d44bb9c047eba64ef2c90f2fc18
sohail <sohailk1999@gmail.com>
2023-03-06 23:28:46 +0530

    testing

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
 
-from djangp.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,7 +11,7 @@ def create_user(**params):
     """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
-Class PublicUserApiTests(TestCase):
+class PublicUserApiTests(TestCase):
     """Test the public features of the user API."""
 
     def setUp(self):
@@ -30,21 +30,21 @@ Class PublicUserApiTests(TestCase):
         self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists_error(self):
-        payload = (
+        payload = {
             'email': 'test@example.com',
-            'password': 'testpass123',
-            'name': 'Test Name'
-        )
+            'password':  'testpass123',
+            'name': 'Test Name',
+        }
         create_user(**payload)
         res = self.client.get(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
     
     def test_password_too_short(self):
-        payload = (
+        payload = {
             'email': 'test2@example.com',
-            'password': 'tes',
-            'name': 'Test Name'
-        )
+            'password':  'te',
+            'name': 'Test Name',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
